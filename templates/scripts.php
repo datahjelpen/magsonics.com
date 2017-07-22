47ee63c82754113ab18c506b304296c9ed526c77
Bjørnar Hagen <bjornar@datahjelpen.org>
2017-07-22 16:11:38 +0000

    Various changes

--- templates/scripts.php
+++ templates/scripts.php
@@ -1,13 +1,5 @@
 <script src="https://cdn.jsdelivr.net/jquery/3.2.1/jquery.min.js"></script>
-<div id="mobilecheck"></div>
 <script>
-    window.addEventListener('load', function() {
-        window.is_mobile = false;
-        if (window.getComputedStyle(document.getElementById("mobilecheck")).getPropertyValue("content") == "\"mobile\"") {
-            window.is_mobile = true;
-        }
-    });
-
     (function($){
         $(function () {
             $('#content').css('visibility', 'visible');
