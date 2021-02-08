66c71bd6b097b79d2a2f1d91eab69b069ccc0aea
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-02-08 11:39:37 +0100

    Migrate from php to static html

--- templates/footer.php
+++
@@ -1,8 +0,0 @@
-    <?php require_once 'fonts.php'; ?>
-    <?php require_once 'scripts.php'; ?>
-    <footer>
-        <p>(C) Copyright <?= date('Y') . ' - ' . $brand->name_official; ?></p>
-        <p>Made by <a href="https://datahjelpen.no/?ref=magsonics-footer">Datahjelpen AS</a></p>
-    </footer>
-</body>
-</html>
